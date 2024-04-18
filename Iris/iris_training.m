986525e242d08582d0d5807e4ac4d6438983a9f0
innepils <simon@veldig.travel>
2024-04-18 13:31:45 +0200

    Updated iris_training

--- Iris/iris_training.m
+++ Iris/iris_training.m
@@ -1,10 +1,10 @@
 
 %% Initialize data set
-x1all = load('Data/class_1', '-ascii');
-x2all = load('Data/class_2', '-ascii');
-x3all = load('Data/class_3', '-ascii');
+x1_all = load('Data/class_1', '-ascii');
+x2_all = load('Data/class_2', '-ascii');
+x3_all = load('Data/class_3', '-ascii');
 
 %% Initialize training set
-x1_training = [x1all(1:30)];
-x2_training = [x2all(1:30,4)];
-x3_training = [x3all(1:30,4)];
+x1_training = [x1all(1:30,:)];
+x2_training = [x2all(1:30,:)];
+x3_training = [x3all(1:30,:)];
