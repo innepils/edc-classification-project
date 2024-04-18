03f2068dd5f8ac4b9fa861cee9646d37ab47cb12
innepils <simon@veldig.travel>
2024-04-18 13:53:29 +0200

    Initialized data, training and test sets

--- Iris/iris_training.m
+++ Iris/iris_training.m
@@ -1,10 +1,27 @@
+clear all
+clc
 
 %% Initialize data set
-x1_all = load('Data/class_1', '-ascii');
-x2_all = load('Data/class_2', '-ascii');
-x3_all = load('Data/class_3', '-ascii');
+c1_all = load('Data/class_1', '-ascii'); % Setosa
+c2_all = load('Data/class_2', '-ascii'); % Versicolor
+c3_all = load('Data/class_3', '-ascii'); % Virginica
+
 
 %% Initialize training set
-x1_training = [x1all(1:30,:)];
-x2_training = [x2all(1:30,:)];
-x3_training = [x3all(1:30,:)];
+N = 30;     % size of training set
+c1_training = [c1_all(1:N,:)];
+c2_training = [c2_all(1:N,:)];
+c3_training = [c3_all(1:N,:)];
+
+
+%% Initialize test set
+M = 20;     % size of test set
+c1_test = [c1_all(N+1:N+M, :)];
+c2_test = [c2_all(N+1:N+M, :)];
+c3_test = [c3_all(N+1:N+M, :)];
+
+
+%% Training parameters
+alpha = 1;     % step factor 
+
+
