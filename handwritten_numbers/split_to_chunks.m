637bf1ffedf22e302a4543f837b55ef68517ae18
innepils <simon@veldig.travel>
2024-04-22 13:37:32 +0200

    work on task 1

---
+++ handwritten_numbers/split_to_chunks.m
@@ -0,0 +1,16 @@
+function [chunked_vec] = split_to_chunks(dataset, partition_index, chunk_size)
+%splitToChunks: 
+%   Detailed explanation goes here
+
+if partition_index == 1
+    i = 1;
+else 
+    i = (partition_index-1)*chunk_size;
+end
+
+
+chunked_vec = dataset(i:i+chunk_size-1, :);
+
+
+end
+
