35986566124e5cdc5676e7befec733d88f444989
EvenFosby <even.fosby@gmail.com>
2024-04-22 15:26:14 +0200

    testing

--- handwritten_numbers/split_to_chuncks.m
+++ handwritten_numbers/split_to_chuncks.m
@@ -1,6 +1,6 @@
 function [chuncked_vec] = split_to_chuncks(dataset, partition_index, chunck_size)
-%splitToChuncks: 
-%   Detailed explanation goes here
+%splitToChuncks: reduces the full dataset into a reduced dataset of size
+%chunck_size, at index partition_index.
 
 if partition_index == 1
     i = 1;
@@ -8,9 +8,5 @@ else
     i = (partition_index-1)*chunck_size;
 end
 
-
 chuncked_vec = dataset(i:i+chunck_size-1, :);
-
-
 end
-
