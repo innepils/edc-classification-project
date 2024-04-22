95de5d9705af2f5bc387804724b807e32ae79b38
innepils <simon@veldig.travel>
2024-04-22 13:37:32 +0200

    work on task 1

--- handwritten_numbers/handwritten_numbers_classifier.m
+++ handwritten_numbers/handwritten_numbers_classifier.m
@@ -11,11 +11,12 @@ load('data/data_all.mat');
 
 % Split data set into chunks of images
 chunk_size = 1000;
+training_set = split_to_chunks(trainv, 1, chunk_size);
 
 %% NN-based classifier using the Euclidian distance
 
 
-% Euclidian distance decision rule
+% Euclidian distance
 function d = euc_dist(x, mu)
     d = (x-mu)' * (x-mu);
 end
