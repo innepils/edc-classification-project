f6609eec5e0afaae840d51effe8cbc22f5905661
innepils <simon@veldig.travel>
2024-04-22 15:25:21 +0200

    changes in handwritten_numbers

--- handwritten_numbers/handwritten_numbers_classifier.m
+++ handwritten_numbers/handwritten_numbers_classifier.m
@@ -4,7 +4,7 @@ close all
 
 %% Initialization
 % Constant values
-M = 1;      % Initial number of clusters
+num_classes = 10;
 
 % Initialize data set
 load('data/data_all.mat');
@@ -13,11 +13,21 @@ load('data/data_all.mat');
 chunk_size = 1000;
 training_set = split_to_chunks(trainv, 1, chunk_size);
 
-%% NN-based classifier using the Euclidian distance
+%% Task 2a - NN-based classifier using the Euclidian distance
 
 
-% Euclidian distance
-function d = euc_dist(x, mu)
-    d = (x-mu)' * (x-mu);
-end
-    
+
+%% Task 2b - Plotting
+% x = zeros(28,28);
+% x(:) = testv(1,:);
+% image(x');
+% 
+% result = dist(trainv(15,:)', trainv(15,:));
+
+
+%% FAST SOLUTION
+% model = fitcknn(trainv,trainlab);
+% i = randi([1,1000], 1);
+% new_observation = testv(i,:);
+% predicted_label = predict(model, new_observation);
+% disp(predicted_label);
