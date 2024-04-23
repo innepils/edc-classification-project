f6089523fbac722ae414b15dd25aa46763225a69
EvenFosby <even.fosby@gmail.com>
2024-04-23 09:36:22 +0200

    removed knn func

--- handwritten_numbers/knn.m
+++
@@ -1,8 +0,0 @@
-function [confusion_matrix, results] = knn(train_data, test_data, train_lab, test_lab, k,num_classes)
-
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-
-    
-end
-
