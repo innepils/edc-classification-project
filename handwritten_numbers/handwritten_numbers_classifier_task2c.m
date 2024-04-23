028ff912ef2025b21a500d5f312fdfce57e93cd5
innepils <simon@veldig.travel>
2024-04-23 15:17:15 +0200

    Completed task 2c, experimenting with k-parameter

--- handwritten_numbers/handwritten_numbers_classifier_task2c.m
+++ handwritten_numbers/handwritten_numbers_classifier_task2c.m
@@ -7,7 +7,7 @@ tic
 % Constant values
 num_classes = 10;
 M = 64;             % number of clusters
-
+k = 1;              % number of nearest neighbors
 % Initialize data set
 load('data/data_all.mat');
 
@@ -16,7 +16,7 @@ load('data/data_all.mat');
 [trainlab_sorted, sortIdx] = sort(trainlab, 'descend');
 trainv_sorted = trainv(sortIdx, :);
 
-% Generate centroids (clusters)
+% Generate centroids
 centroids_vec = zeros(M*num_classes, vec_size);
 
 for i = 1:num_classes
@@ -26,17 +26,16 @@ end
 
 % Generate centroids labels
 centroids_lab = zeros(size(centroids_vec,1),1);
-k = num_classes - 1;
+class_counter = num_classes - 1;
 
 for j = 1:num_classes
-    centroids_lab(j*M-M+1:j*M, :) = k;
-    k = k-1;
+    centroids_lab(j*M-M+1:j*M, :) = class_counter;
+    class_counter = class_counter-1;
 end 
 
 %% NN-based classifier using the Euclidian distance
 
 confusion_matrix = zeros(num_classes, num_classes);
-incorrect = [];
 
 % Train on the whole training set
 train_images = centroids_vec;
@@ -55,11 +54,12 @@ for j = 1:num_test_samples
     % Compute euclidean distance
     distances = sum((train_images - test_images(j, :)).^2, 2);
 
-    % Find nearest neighbor
-    [~, nn] = min(distances);
+    % Find k nearest neighbors
+    [~, nn] = mink(distances, k);
 
     % Predicted label based on nearest neighbor
-    predicted_label = train_labels(nn);
+    predicted_labels = train_labels(nn);
+    predicted_label = mode(predicted_labels);
 
     % True label
     true_label = test_labels(j);
@@ -69,10 +69,6 @@ for j = 1:num_test_samples
 
     waitbar(j/num_test_samples, waiting, sprintf('Progress: %d %%', floor(j/num_test_samples*100)));
 
-    % Check if misclassified
-    if true_label ~= predicted_label
-        incorrect = [incorrect; j, true_label, predicted_label];
-    end
 end
 
 % Compute error rate
@@ -83,4 +79,3 @@ disp('Confusion Matrix:');
 disp(confusion_matrix);
 fprintf('Error Rate: %.2f%%\n', error_rate * 100);
 toc
-
