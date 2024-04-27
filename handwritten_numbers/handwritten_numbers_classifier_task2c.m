8a7a7068510fccf11842218e91a4c1540b0243f2
EvenFosby <even.fosby@gmail.com>
2024-04-27 16:48:51 +0200

    made HWN ready for delivery

--- handwritten_numbers/handwritten_numbers_classifier_task2c.m
+++ handwritten_numbers/handwritten_numbers_classifier_task2c.m
@@ -1,13 +1,20 @@
-clear all
-clc
-close all
+%   Description: KNN-algorithem for classification of the MNIST data set. 
+%                The classifier is trained using 60 000 training images 
+%                and 10 000 test images.
+%
+%   Author: E. T. Fosby, S. Klovning
+%   
+%   Date: 26-04-2024
+
+clear all; clc; close all
 
 tic
 %% Initialization
 % Constant values
-num_classes = 10;
+num_classes = 10;   % number of classes
 M = 64;             % number of clusters
 k = 7;              % number of nearest neighbors
+
 % Initialize data set
 load('data/data_all.mat');
 
@@ -20,7 +27,8 @@ trainv_sorted = trainv(sortIdx, :);
 centroids_vec = zeros(M*num_classes, vec_size);
 
 for i = 1:num_classes
-    reduced_trainv_sorted = split_to_chunks(trainv_sorted, i, size(trainv_sorted,1)/num_classes);
+    reduced_trainv_sorted = split_to_chunks(trainv_sorted, i, ...
+        size(trainv_sorted,1)/num_classes);
     [~, centroids_vec(i*M-M+1:i*M, :)] = kmeans(reduced_trainv_sorted, M);
 end
 
@@ -37,6 +45,7 @@ end
 %% NN-based classifier using the Euclidian distance
 
 confusion_matrix = zeros(num_classes, num_classes);
+incorrect = [];
 
 % Train on the whole training set
 train_images = centroids_vec;
@@ -47,9 +56,6 @@ num_test_samples = 10000;
 test_images = testv(1:num_test_samples, :);
 test_labels = testlab(1:num_test_samples);
 
-% Initialize waitbar
-waiting = waitbar(0, 'Starting');
-
 % Iterate over test images
 for j = 1:num_test_samples
     % Compute euclidean distance
@@ -57,6 +63,9 @@ for j = 1:num_test_samples
 
     % Find k nearest neighbors
     [~, nn] = mink(distances, k);
+
+    % Find nearest neighbor (NN-algorithem, task 1)
+    % [~, nn] = min(distances); 
 
     % Predicted label based on nearest neighbor
     predicted_labels = train_labels(nn);
@@ -66,9 +75,13 @@ for j = 1:num_test_samples
     true_label = test_labels(j);
 
     % Update confusion matrix
-    confusion_matrix(true_label + 1, predicted_label + 1) = confusion_matrix(true_label + 1, predicted_label + 1) + 1;
-
-    waitbar(j/num_test_samples, waiting, sprintf('Progress: %d %%', floor(j/num_test_samples*100)));
+    confusion_matrix(true_label + 1, predicted_label + 1) = ...
+    confusion_matrix(true_label + 1, predicted_label + 1) + 1;
+    
+    % Check if misclassified
+    if true_label ~= predicted_label
+        incorrect = [incorrect; j, true_label, predicted_label];
+    end
 
 end
 
@@ -80,9 +93,4 @@ disp('Confusion Matrix:');
 disp(confusion_matrix);
 fprintf('Error Rate: %.2f%%\n', error_rate * 100);
 toc
-
-
-cm = confusionchart(confusion_matrix,{'0','1','2','3','4','5','6','7','8','9'});
-cm.Title = 'Clustering, K=7';
-cm.FontSize = 13;
 
